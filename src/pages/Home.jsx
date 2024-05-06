191dbeb17be97b6b86e72d8d1c24bc408a59435b
busra <kocarslanbusrab@gmail.com>
2024-05-06 06:10:25 +0300

    grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,22 +16,21 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      {newsData?.map((item, key) => (
-        <article className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm" key={key}>
-          <img src={item?.urlToImage} loading="lazy" alt={item?.title} className="w-full h-48 rounded-t-md" />
-          <div className="flex items-center mt-2 pt-3 ml-4 mr-2">
-            <div className="flex-none w-10 h-10 rounded-full"></div>
-            <div className="ml-3">
-            <h3 className="text-xl text-gray-900">{item?.title}</h3>
-            <span className="block text-gray-900">{item?.content}</span>
-            </div>
-          </div>
-          <div className="pt-3 ml-4 mr-2 mb-3">
-           
-           
-          </div>
-        </article>
-      ))}
+      <div className='w-[90%] m-auto grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+  {newsData?.map((item, key) => (
+    <article className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm" key={key}>
+      <img src={item?.urlToImage} loading="lazy" alt={item?.title} className="w-full h-48 rounded-t-md" />
+      <div className="flex items-center mt-2 pt-3 ml-4 mr-2">            
+        <div className="ml-3">
+          <h3 className="text-xl text-gray-900">{item?.title}</h3>
+          <span className="block text-gray-900">{item?.content}</span>
+        </div>
+      </div>
+      <div className="pt-3 ml-4 mr-2 mb-3"></div>
+    </article>
+  ))}
+</div>
+
     </div>
   );
 }
